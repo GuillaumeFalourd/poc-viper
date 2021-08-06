dfaae034f5e3950538bd1b2da37fac5acf9868c5
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-08-06 15:49:23 -0300

    update viper.go and *.yaml files
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- README.md
+++ README.md
@@ -11,8 +11,69 @@ References:
 - Golang installed
 - Viper library installed: `go get github.com/spf13/viper`
 
+## Usage
+
+This implementation will **read** fields from a `.yaml` file located at the repository `root` according to the following format:
+
+### For REST API
+
+```yaml
+name: Test Viper REST
+id: 123
+inputs:
+  request:
+    api: REST
+    method: "POST"
+    url: "http://localhost:8080"
+    headers:  # You can use any custom-fields inside this headers map
+      authorization: "Bearer token"
+      accept: "application/json"
+    body:  # You can use any custom-fields inside this body map
+      document: "123456YZ"
+      email: "test@example.com"
+```
+
+### For gRPC API
+
+```yaml
+name: Test Viper gRPC
+id: 456
+inputs:
+  request1:
+  request:
+    api: gRPC
+    server-address: "http://localhost:9090"
+    service: "LoginRequest"
+    fields: # You can use any custom-fields inside this fields map
+      document: "78901AB"
+      email: "abcde@test.com"
+```
+
+To set the `*.yaml` to read, update the viper.go line 11: `vi.SetConfigFile("{name}.yaml")`
+
+_The default value for the yaml file is `test-rest.yaml`._
+
 ## Run
 
 At the repository `root` directory run: `go run ./viper.go`
 
-_Note: This will **read** fields from the `test.yaml` file at the repository `root`._
+### For the REST API yaml
+
+```bash
+Name: Test Viper REST
+Id: 123
+Method: POST
+Url: http://localhost:8080
+Headers: map[accept:application/json authorization:Bearer token]
+Body: map[document:123456YZ email:test@example.com]
+```
+
+### For the gRPC API yaml
+
+```bash
+Name: Test Viper gRPC
+Id: 456
+Server Address: http://localhost:9090
+Service: LoginRequest
+Fields: map[document:78901AB email:abcde@test.com]
+```
--- viper.go
+++ viper.go
@@ -8,32 +8,105 @@ import (
 
 func main() {
 	vi := viper.New()
-	vi.SetConfigFile("test.yaml")
+	vi.SetConfigFile("test-rest.yaml")
 	vi.ReadInConfig()
 
 	// Using https://github.com/spf13/viper#getting-values-from-viper
-	fmt.Println("Is name set? ", vi.IsSet("name"))
-	fmt.Println("Is id set? ", vi.IsSet("id"))
-	fmt.Println("Is inputs set? ", vi.IsSet("inputs"))
-	fmt.Println("Is api set? ", vi.IsSet("api"))
-	fmt.Println("----- First Level -----")
-	fmt.Println("Name Value:", vi.GetString("name"))
-	fmt.Println("Id Value:", vi.GetInt("id"))
-	fmt.Println("Inputs Map:", vi.Get("inputs"))
-	fmt.Println("----- Second Level -----")
-	fmt.Println("Inputs-Request1:", vi.Get("inputs.request1"))
-	fmt.Println("----- Third Level -----")
-	fmt.Println("Inputs-Request1-Method Value:", vi.Get("inputs.request1.method"))
-	fmt.Println("Inputs-Request1-Url Value:", vi.Get("inputs.request1.url"))
-	fmt.Println("Inputs-Request1-Headers Map:", vi.Get("inputs.request1.headers"))
-	fmt.Println("Inputs-Request1-Body Map:", vi.Get("inputs.request1.body"))
-	fmt.Println("----- Second Level -----")
-	fmt.Println("Request2:", vi.Get("inputs.request2"))
-	fmt.Println("----- Third Level -----")
-	fmt.Println("Inputs-Request2-Method Value:", vi.Get("inputs.request2.method"))
-	fmt.Println("Inputs-Request2-Url Value:", vi.Get("inputs.request2.url"))
-	fmt.Println("Inputs-Request2-Headers Map:", vi.Get("inputs.request2.headers"))
-	fmt.Println("Inputs-Request2-Body Map:", vi.Get("inputs.request2.body"))
-	fmt.Println("-----")
-	fmt.Println("All:", vi.AllSettings())
+
+	if validFormat(vi) {
+		// Extract YAML first level parameters
+		name := vi.Get("name")
+		id := vi.Get("id")
+
+		// Any Implementation
+		fmt.Println("Name:", name)
+		fmt.Println("Id:", id)
+
+		api := vi.Get("inputs.request.api")
+		if api == "REST" {
+			// Extract request parameters
+			method := vi.Get("inputs.request.method")
+			url := vi.Get("inputs.request.url")
+			headers := vi.Get("inputs.request.headers")
+			body := vi.Get("inputs.request.body")
+
+			// Any Implementation
+			fmt.Println("Method:", method)
+			fmt.Println("Url:", url)
+			fmt.Println("Headers:", headers)
+			fmt.Println("Body:", body)
+		}
+		if api == "gRPC" {
+			// Extract request parameters
+			serverAddress := vi.Get("inputs.request.server-address")
+			service := vi.Get("inputs.request.service")
+			fields := vi.Get("inputs.request.fields")
+
+			// Any Implementation
+			fmt.Println("Server Address:", serverAddress)
+			fmt.Println("Service:", service)
+			fmt.Println("Fields:", fields)
+		}
+	} else {
+		fmt.Println("Please, check the YAML format on the repository README.md file!")
+	}
+}
+
+func validFormat(vi *viper.Viper) bool {
+	// Check YAML first level parameters
+	if !vi.IsSet("name") {
+		fmt.Println("ERROR yaml format: Name")
+		return false
+	}
+	if !vi.IsSet("id") {
+		fmt.Println("ERROR yaml format: Id")
+		return false
+	}
+	if !vi.IsSet("inputs") {
+		fmt.Println("ERROR yaml format: Inputs")
+		return false
+	}
+	// Check YAML request parameter
+	if !vi.IsSet("inputs.request") {
+		fmt.Println("ERROR yaml format: Request")
+		return false
+	}
+	api := vi.Get("inputs.request.api")
+	if api == "REST" {
+		// Check REST request parameters
+		if !vi.IsSet("inputs.request.method") {
+			fmt.Println("ERROR request format: Method")
+			return false
+		}
+		if !vi.IsSet("inputs.request.url") {
+			fmt.Println("ERROR request format: Url")
+			return false
+		}
+		if !vi.IsSet("inputs.request.headers") {
+			fmt.Println("ERROR request format: Headers")
+			return false
+		}
+		if !vi.IsSet("inputs.request.body") {
+			fmt.Println("ERROR request format: Body")
+			return false
+		}
+	}
+
+	if api == "gRPC" {
+		// Check gRPC request parameters
+		if !vi.IsSet("inputs.request.server-address") {
+			fmt.Println("ERROR request format: Server Address")
+			return false
+		}
+		if !vi.IsSet("inputs.request.service") {
+			fmt.Println("ERROR request format: Service")
+			return false
+		}
+		if !vi.IsSet("inputs.request.fields") {
+			fmt.Println("ERROR request format: Fields")
+			return false
+		}
+	}
+
+	return true
 }
